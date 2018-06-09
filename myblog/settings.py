d2a3c6f24491f5b91a769963b663ff993e8fe31d
JAG1888 <ab.jasinska@gmail.com>
2018-06-09 16:08:58 +0200

    dodanie widoku i modelu dla postu

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@sw&i-rn28r#jt@yy!ph+zffnhfry*7@a&4sq&jh=p0a1j#!b8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
